623828a1eda80ec0c650425c1e1ac9fe0b0683dc
AaLin-Git <aalin.box.it@gmail.com>
2023-02-01 11:11:26 +0100

    feat: write a function for tower sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let newArr = [];
+    console.log(matrix[1]);
+    matrix.map((item) => {
+        console.log(matrix.indexOf(item));
+        if (matrix.indexOf(item) % 2 === 0) {
+            newArr = newArr.concat(item);
+        } else {
+            newArr = newArr.concat(item.reverse());
+        }
+
+        console.log(newArr);
+    });
+    return newArr;
+};
