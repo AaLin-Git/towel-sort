1c7a742818c3d3d3673bc82e958e3211de43a484
AaLin-Git <aalin.box.it@gmail.com>
2023-02-01 11:21:11 +0100

    feat: finish function and clean code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
-    let newArr = [];
-    console.log(matrix[1]);
-    matrix.map((item) => {
-        console.log(matrix.indexOf(item));
-        if (matrix.indexOf(item) % 2 === 0) {
-            newArr = newArr.concat(item);
-        } else {
-            newArr = newArr.concat(item.reverse());
-        }
-
-        console.log(newArr);
-    });
-    return newArr;
+    if (matrix === undefined) {
+        return [];
+    } else {
+        let newArr = [];
+        matrix.forEach((item) => {
+            if (matrix.indexOf(item) % 2 === 0) {
+                newArr = newArr.concat(item);
+            } else {
+                newArr = newArr.concat(item.reverse());
+            }
+        });
+        return newArr;
+    }
 };
